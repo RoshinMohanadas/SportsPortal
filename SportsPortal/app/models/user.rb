194a229007051109b2bd1d7c2e380e49ea7d04d5
xavierjose93 <xavier.jose55@gmail.com>
2019-10-16 18:48:25 +0530

    added user validations

--- SportsPortal/app/models/user.rb
+++ SportsPortal/app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+    
+  validates :username, uniqueness: true, presence: true
+  validates :password, length: { minimum: 6 }
+  validates :email, uniqueness: true, presence: true
+  validates :role, presence: true
+  validates :contactNo, presence: true
+  validates :name, presence: true
+
 end
