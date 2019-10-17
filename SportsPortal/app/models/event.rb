9cfdcf7d6382d833e518a9e514597d035311b317
Roshin Mohanadas <roshinmohanadas@yahoo.in>
2019-10-17 19:22:57 +0530

    has_many added in event and team model

--- SportsPortal/app/models/event.rb
+++ SportsPortal/app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :eventgroup
+  has_many :teams
+  has_many :matches
 end
--- SportsPortal/app/models/team.rb
+++ SportsPortal/app/models/team.rb
@@ -1,4 +1,5 @@
 class Team < ApplicationRecord
   belongs_to :event
   belongs_to :user
+  has_many :teammembers
 end
