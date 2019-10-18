cb45315dae0e12e425195e8ea1b9b8d5fd92b970
Roshin Mohanadas <roshinmohanadas@yahoo.in>
2019-10-18 19:45:55 +0530

    edit team done

--- SportsPortal/config/routes.rb
+++ SportsPortal/config/routes.rb
@@ -16,8 +16,12 @@ Rails.application.routes.draw do
 
 
   get 'teams/', to: 'teams#index'
+  get 'teams/show', to: 'teams#show'
   get 'teams/basicinfo', to: 'teams#basicinfo'
+  get 'teams/editbasicinfo', to: 'teams#editbasicinfo'
   post 'teams/createbasicinfo', to: 'teams#createbasicinfo'
+  post 'teams/createteam', to: 'teams#createteam'
+  patch 'teams/updatebasicinfo', to: 'teams#updatebasicinfo'
 
   get 'teammembers/newmember', to: 'teammembers#newmember'
   post 'teammembers/createmember', to: 'teammembers#createmember'
