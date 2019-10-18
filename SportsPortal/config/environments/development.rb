0059c7a22f44945c33f0030dae90c889691ff9f2
xavierjose93 <xavier.jose55@gmail.com>
2019-10-18 15:52:03 +0530

    mailer

--- SportsPortal/config/environments/development.rb
+++ SportsPortal/config/environments/development.rb
@@ -59,4 +59,31 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.delivery_method = :sendmail
+  # Defaults to:
+  # config.action_mailer.sendmail_settings = {
+  #   location: '/usr/sbin/sendmail',
+  #   arguments: '-i'
+  # }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'no.reply.sportsportal@gmail.com'}
+
+  config.action_mailer.delivery_method = :smtp
+  host= 'localhost:3000'
+  config.action_mailer.default_url_options = {
+    :host => '127.0.0.1',
+      :port => 3000
+   }
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'example.com',
+  user_name:            'no.reply.sportsportal@gmail.com',
+  password:             'sports@nitc',
+  authentication:       'plain',
+  enable_starttls_auto: true  }
+
+
 end
