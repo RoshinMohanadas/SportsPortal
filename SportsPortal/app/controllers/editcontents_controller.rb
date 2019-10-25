bee15b2eb1159bd75702d07b787b406f8c06d843
vsidharth <sidharth.valsaraj@gmail.com>
2019-10-25 19:44:54 +0530

    edit content

---
+++ SportsPortal/app/controllers/editcontents_controller.rb
@@ -0,0 +1,74 @@
+class EditcontentsController < ApplicationController
+  before_action :set_editcontent, only: [:show, :edit, :update, :destroy]
+
+  # GET /editcontents
+  # GET /editcontents.json
+  def index
+    @editcontents = Editcontent.all
+  end
+
+  # GET /editcontents/1
+  # GET /editcontents/1.json
+  def show
+  end
+
+  # GET /editcontents/new
+  def new
+    @editcontent = Editcontent.new
+  end
+
+  # GET /editcontents/1/edit
+  def edit
+  end
+
+  # POST /editcontents
+  # POST /editcontents.json
+  def create
+    @editcontent = Editcontent.new(editcontent_params)
+
+    respond_to do |format|
+      if @editcontent.save
+        format.html { redirect_to @editcontent, notice: 'Editcontent was successfully created.' }
+        format.json { render :show, status: :created, location: @editcontent }
+      else
+        format.html { render :new }
+        format.json { render json: @editcontent.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /editcontents/1
+  # PATCH/PUT /editcontents/1.json
+  def update
+    respond_to do |format|
+      if @editcontent.update(editcontent_params)
+        format.html { redirect_to @editcontent, notice: 'Editcontent was successfully updated.' }
+        format.json { render :show, status: :ok, location: @editcontent }
+      else
+        format.html { render :edit }
+        format.json { render json: @editcontent.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /editcontents/1
+  # DELETE /editcontents/1.json
+  def destroy
+    @editcontent.destroy
+    respond_to do |format|
+      format.html { redirect_to editcontents_url, notice: 'Editcontent was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_editcontent
+      @editcontent = Editcontent.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def editcontent_params
+      params.require(:editcontent).permit(:welcometext, :about, :facilities, :contact)
+    end
+end
--- SportsPortal/db/schema.rb
+++ SportsPortal/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_24_165841) do
+ActiveRecord::Schema.define(version: 2019_10_25_135455) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 2019_10_24_165841) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "editcontents", force: :cascade do |t|
+    t.string "welcometext"
+    t.text "about"
+    t.text "facilities"
+    t.text "contact"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "eventgroups", force: :cascade do |t|
