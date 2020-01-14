2dabd154c9f44f7eae75df562411d532e7886626
Xavier Jose <xavier.jose55@gmail.com>
2020-01-14 11:05:14 +0530

    email

--- SportsPortal/app/models/user.rb
+++ SportsPortal/app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
 	#validates :ename,  presence: true, length: { maximum: 50 }
 	# VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-	validates :email,  presence: true, length: { maximum: 225 },
+	validates :email, length: { maximum: 225 },
 					   format: { with: VALID_EMAIL_REGEX },
 					   uniqueness: { case_sensitive: false }
 	validates :contactno,  presence: true, length: { minimum: 10,maximum: 10 }
